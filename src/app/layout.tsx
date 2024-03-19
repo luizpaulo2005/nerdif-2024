8c150f6d27370ba44699c645281aefb41d55d8ff
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-03-19 12:57:32 -0300

    feat: add toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" suppressHydrationWarning>
+    <html lang="pt-br" suppressHydrationWarning={true} >
       <AuthProvider>
         <DataProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <body className={inter.className}>
+              <Toaster richColors />
               <Navbar />
               {children}
             </body>
