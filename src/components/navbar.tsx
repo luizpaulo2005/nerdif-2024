85d702e2a5a1e70a9ace69dab986a17ea0a24c88
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-04-22 14:07:49 -0300

    fix:  navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,21 @@
+"use client"
+
 import { User } from "@/components/user";
 import Image from "next/image";
 import Logo from "/public/logo.png";
 import { CreateTeamForm } from "@/components/create-team-form";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
+  const { status } = useSession()
+
   return (
     <div className="flex justify-around p-2">
       <div className="flex items-center gap-2">
         <span className="font-semibold">NerdIF 2024</span>
         <Image className="size-10 rounded-md" src={Logo} alt="Logo" />
       </div>
-      <CreateTeamForm />
+      {status === "authenticated" && <CreateTeamForm />}
       <User />
     </div>
   );
