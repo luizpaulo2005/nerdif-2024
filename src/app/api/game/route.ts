cfea332a3c31b7dac721204bc9faf481fe9d205d
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-04-26 00:28:02 -0300

    fix: turn game route public

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -1,12 +1,7 @@
-import { authOptions } from "@/lib/auth-options";
 import { prisma } from "@/lib/prisma";
-import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 const getGames = async (req: Request) => {
-  const session = await getServerSession({ req, ...authOptions });
-
-  if (session) {
     const select = await prisma.game.findMany();
 
     if (select) {
@@ -14,9 +9,6 @@ const getGames = async (req: Request) => {
     } else {
       return NextResponse.json({ message: "No games found" }, { status: 404 });
     }
-  } else {
-    return NextResponse.json({ message: "Unauthenticated" }, { status: 401 });
   }
-};
 
 export { getGames as GET };
